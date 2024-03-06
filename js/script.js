ce6d1d1b181661b06e22724946c8b818eadd8918
Pavel Butkariov <butkarevpavel@gmail.com>
2024-03-06 14:15:19 +0300

    add search window on click to search button

--- js/script.js
+++ js/script.js
@@ -1,3 +1,45 @@
+// Бургер-меню в header
+let burger = document.querySelector('.burger');
+let menu = document.querySelector('.header__nav');
+let menuLinks = menu.querySelectorAll('.header__link');
+
+burger.addEventListener('click', function () {
+  burger.classList.toggle('burger--active');
+  menu.classList.toggle('header__nav--active');
+  document.body.classList.toggle('stop-scroll');
+});
+
+menuLinks.forEach(function (el) {
+  el.addEventListener('click', function () {
+    burger.classList.remove('burger--active');
+    menu.classList.remove('header__nav--active');
+    document.body.classList.remove('stop-scroll');
+  });
+});
+
+// Форма поиска по сайту в header
+const searchBtn = document.querySelector('.search-btn');
+const searchForm = document.querySelector('.header__form');
+const searchInput = document.querySelector('.header__input');
+const closeBtn = document.querySelector('.close-btn');
+const main = document.querySelector('.main');
+
+searchBtn.addEventListener('click', function () {
+  searchForm.classList.add('header__form--active');
+  searchBtn.classList.add('search-btn--active');
+
+  searchForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+  });
+
+  closeBtn.addEventListener('click', function (ev) {
+    ev.preventDefault();
+    searchForm.classList.remove('header__form--active');
+    searchBtn.classList.remove('search-btn--active');
+  });
+});
+
+// Слайдер секции hero
 const swiper = new Swiper('.swiper', {
   slidesPerView: 1,
   loop: true,
@@ -10,48 +52,26 @@ const swiper = new Swiper('.swiper', {
   }
 });
 
-let burger = document.querySelector('.burger');
-let menu = document.querySelector('.header__nav');
-let menuLinks = menu.querySelectorAll('.header__link')
-
-burger.addEventListener('click', function() {
-
-  burger.classList.toggle('burger--active');
-
-  menu.classList.toggle('header__nav--active');
-
-  document.body.classList.toggle('stop-scroll');
-})
-
-menuLinks.forEach(function (el) {
-  el.addEventListener('click', function () {
-
-    burger.classList.remove('burger--active');
-
-    menu.classList.remove('header__nav--active');
-
-    document.body.classList.remove('stop-scroll');
-  })
-})
-
+// Табы секции stages
 let tabsBtn = document.querySelectorAll('.stages__btn');
 let tabsItem = document.querySelectorAll('.stages__box');
 
-tabsBtn.forEach(function(element){
-  element.addEventListener('click', function(e){
+tabsBtn.forEach(function (element) {
+  element.addEventListener('click', function (e) {
     const path = e.currentTarget.dataset.path;
 
-    tabsBtn.forEach(function(btn){ btn.classList.remove('stages__btn--active')});
+    tabsBtn.forEach(function (btn) { btn.classList.remove('stages__btn--active') });
     e.currentTarget.classList.add('stages__btn--active');
 
-    tabsItem.forEach(function(element){ element.classList.remove('stages__box--active')});
+    tabsItem.forEach(function (element) { element.classList.remove('stages__box--active') });
     document.querySelector(`[data-target="${path}"]`).classList.add('stages__box--active');
   });
 });
 
-$( function() {
-  $( "#accordion" ).accordion({
+// Аккордеон секции faq
+$(function () {
+  $("#accordion").accordion({
     collapsible: true,
     active: false,
   });
-} );
+});
